b9b82b696aefdb55d97899a2d9571f05f7b2900b
Fellipe <fellipeapires@gmail.com>
2023-05-16 19:56:40 -0300

    refatoracao resource permissao remover permissao usuario

--- src/main/java/br/com/sil/resource/PermissaoResource.java
+++ src/main/java/br/com/sil/resource/PermissaoResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -67,7 +66,7 @@ public class PermissaoResource implements IPermissaoResource {
 		return new ResponseEntity<Integer>(qtd, HttpStatus.OK);
 	}
 	
-	@DeleteMapping("/removerusuario")
+	@PostMapping("/removerusuario")
 	public ResponseEntity<?> removerPermissaoUsuario(@RequestBody PermissaoDto entity) {
 		Integer qtdDeletado = this.permissaoService.removerPermissaoUsuario(entity);
 		return new ResponseEntity<>(qtdDeletado, HttpStatus.OK);
