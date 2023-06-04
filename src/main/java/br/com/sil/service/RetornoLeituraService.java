32b89d5de83ce573295c6268bb91f34875389772
Fellipe <fellipeapires@gmail.com>
2023-06-04 14:57:48 -0300

    refatoracao pesquisa retorno leitura cliente

--- src/main/java/br/com/sil/service/RetornoLeituraService.java
+++ src/main/java/br/com/sil/service/RetornoLeituraService.java
@@ -407,32 +407,36 @@ public class RetornoLeituraService implements IRetornoLeituraService {
 	public RetornoLeituraClienteDto getRetornoLeituraCliente(RetornoLeituraFilter filter) {
 		RetornoLeituraClienteProjection projection = this.retornoLeituraRepository.getRetornoLeituraCliente(filter.getIdRegional(), filter.getDataReferencia(), filter.getGrupoFaturamento(), filter.getInstalacao(), filter.getMedidor());
 		RetornoLeituraClienteDto dto = new RetornoLeituraClienteDto();
-		dto.setIdLeitura(projection.getIdLeitura());
-		dto.setInstalacao(projection.getInstalacao());
-		dto.setMedidor(projection.getMedidor());
-		dto.setLeituraMedida(projection.getLeituraMedida());
-		dto.setDataLeitura(projection.getDataLeitura());
-		dto.setRamoAtividade(projection.getRamoAtividade().toUpperCase());
-		dto.setEndereco(projection.getEndereco());
-		dto.setComplemento(projection.getComplemento());
-		dto.setMunicipio(projection.getMunicipio());
-		dto.setIsFoto(projection.getIsFoto());
-		dto.setLatitude(projection.getLatitude());
-		dto.setLongitude(projection.getLongitude());
-		dto.setListaFoto(this.retornoFotoService.listar(projection.getIdLeitura()));
-		if (projection.getSegmento().equalsIgnoreCase("01")) {
-			dto.setSegmento("RESIDENCIAL");
-		} else if (projection.getSegmento().equalsIgnoreCase("02")) {
-			dto.setSegmento("COMERCIO");
-		} else if (projection.getSegmento().equalsIgnoreCase("13")) {
-			dto.setSegmento("COLETIVO");
-		} else if (projection.getSegmento().equalsIgnoreCase("19")) {
-			dto.setSegmento("REFRIGERACAO");
+		if (projection != null) {
+			dto.setIdLeitura(projection.getIdLeitura());
+			dto.setInstalacao(projection.getInstalacao());
+			dto.setMedidor(projection.getMedidor());
+			dto.setLeituraMedida(projection.getLeituraMedida());
+			dto.setDataLeitura(projection.getDataLeitura());
+			dto.setRamoAtividade(projection.getRamoAtividade().toUpperCase());
+			dto.setEndereco(projection.getEndereco());
+			dto.setComplemento(projection.getComplemento());
+			dto.setMunicipio(projection.getMunicipio());
+			dto.setIsFoto(projection.getIsFoto());
+			dto.setLatitude(projection.getLatitude());
+			dto.setLongitude(projection.getLongitude());
+			dto.setListaFoto(this.retornoFotoService.listar(projection.getIdLeitura()));
+			if (projection.getSegmento().equalsIgnoreCase("01")) {
+				dto.setSegmento("RESIDENCIAL");
+			} else if (projection.getSegmento().equalsIgnoreCase("02")) {
+				dto.setSegmento("COMERCIO");
+			} else if (projection.getSegmento().equalsIgnoreCase("13")) {
+				dto.setSegmento("COLETIVO");
+			} else if (projection.getSegmento().equalsIgnoreCase("19")) {
+				dto.setSegmento("REFRIGERACAO");
+			} else {
+				dto.setSegmento("OUTROS");
+				//(projection.getSegmento().equalsIgnoreCase("03") || projection.getSegmento().equalsIgnoreCase("07") || projection.getSegmento().equalsIgnoreCase("18"))
+			}
+			return dto;
 		} else {
-			dto.setSegmento("OUTROS");
-			//(projection.getSegmento().equalsIgnoreCase("03") || projection.getSegmento().equalsIgnoreCase("07") || projection.getSegmento().equalsIgnoreCase("18"))
+			return null;
 		}
-		return dto;
 	}
 
 }
